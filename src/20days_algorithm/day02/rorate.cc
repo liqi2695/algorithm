fc23b392d79d1defa24f821e600f9eee96b27b55
liqi <2695904557@qq.com>
2021-08-19 11:17:04 +0800

    双指针

---
+++ src/20days_algorithm/day02/rorate.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+
+int main()
+{
+    int k = 11;
+    int size = 6;
+    int s = k / size;
+    cout << s << endl;
+    cout << "hel" << endl;
+}
